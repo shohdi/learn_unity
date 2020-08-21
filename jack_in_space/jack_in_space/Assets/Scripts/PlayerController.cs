89065c90be99cd63b99eaa1e327e493fffba4177
shohdi <shohdi@gmail.com>
2020-08-21 21:17:36 +0300

    connect animator to code

--- jack_in_space/jack_in_space/Assets/Scripts/PlayerController.cs
+++ jack_in_space/jack_in_space/Assets/Scripts/PlayerController.cs
@@ -10,33 +10,45 @@ public class PlayerController : MonoBehaviour
 
     public LayerMask groundLayer;
 
+    public Animator myAnimator;
+
+
 
     void Awake()
     {
         this.rigidBody = this.GetComponent<Rigidbody2D>();
+        this.myAnimator =  this.GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.myAnimator.SetBool("isAlive", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-         
-        if(Input.GetKeyUp(KeyCode.Space))
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
         {
+
             jump();
+
         }
+        this.myAnimator.SetBool("isGrounded", this.isGrounded());
+
+
     }
+
+   
 
     private void jump()
     {
         if (this.isGrounded())
         {
+            this.myAnimator.SetBool("isGrounded", false);
             this.rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
