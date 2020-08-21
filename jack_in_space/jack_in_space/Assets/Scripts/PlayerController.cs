dab20dba92ce48721d8b498f9df559cf28a02a90
shohdi <shohdi@gmail.com>
2020-08-21 15:42:30 +0300

    add isground using raycast and add layer to hit

--- jack_in_space/jack_in_space/Assets/Scripts/PlayerController.cs
+++ jack_in_space/jack_in_space/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 10.0f;
+    
     private Rigidbody2D rigidBody;
+
+    public LayerMask groundLayer;
 
 
     void Awake()
@@ -22,14 +25,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+         
         if(Input.GetKeyUp(KeyCode.Space))
         {
             jump();
         }
     }
 
-    void jump()
+    private void jump()
     {
-        this.rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (this.isGrounded())
+        {
+            this.rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
+
+    private bool isGrounded()
+    {
+        if(Physics2D.Raycast(this.transform.position,Vector2.down,0.2f,groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
