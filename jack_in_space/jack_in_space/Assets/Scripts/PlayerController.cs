7aa2c79b4c1a411dace6418584c9fb695781d86d
shohdi <shohdi@gmail.com>
2020-08-21 15:15:03 +0300

    add new script PlayerController.cs

---
+++ jack_in_space/jack_in_space/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 6.0f;
+    private Rigidbody2D rigidBody;
+
+
+    void Awake()
+    {
+        this.rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            jump();
+        }
+    }
+
+    void jump()
+    {
+        this.rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
