d49312cbe875f8641b06672c61c7193abc0bf26e
shohdi <shohdi@gmail.com>
2020-08-22 19:49:33 +0300

    apply velocity and keep angle

--- jack_in_space/jack_in_space/Assets/Scripts/PlayerController.cs
+++ jack_in_space/jack_in_space/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 10.0f;
+    public float runSpeed = 4f;
+    public Vector3 startingAngle;
     
     private Rigidbody2D rigidBody;
 
@@ -18,12 +20,14 @@ public class PlayerController : MonoBehaviour
     {
         this.rigidBody = this.GetComponent<Rigidbody2D>();
         this.myAnimator =  this.GetComponent<Animator>();
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
         this.myAnimator.SetBool("isAlive", true);
+        this.startingAngle = this.transform.eulerAngles;
     }
 
     // Update is called once per frame
@@ -31,21 +35,29 @@ public class PlayerController : MonoBehaviour
     {
 
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
 
             jump();
 
         }
-
-
+        this.myAnimator.SetBool("isGrounded", this.isGrounded());
+        //Debug.Log(this.isGrounded());
+        //Debug.Log( string.Format("{0},{1}", this.transform.position.x, this.transform.position.y));
 
     }
 
 
     void FixedUpdate()
     {
-        this.myAnimator.SetBool("isGrounded", this.isGrounded());
+        if(this.rigidBody.velocity.x < runSpeed)
+        {
+
+            this.rigidBody.velocity = new Vector2(runSpeed, this.rigidBody.velocity.y);
+            this.rigidBody.angularVelocity = 0f;
+            this.transform.eulerAngles = this.startingAngle;
+
+        }
     }
 
 
@@ -61,7 +73,7 @@ public class PlayerController : MonoBehaviour
 
     private bool isGrounded()
     {
-        if(Physics2D.Raycast(this.transform.position,Vector2.down,0.2f,groundLayer))
+        if(Physics2D.Raycast(this.transform.position,Vector2.down,0.2f,groundLayer.value))
         {
             return true;
         }
